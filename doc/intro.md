b963bbc1f358523996db613d35354bebf4caa8a3
ppodoru <ppodoru@navercorp.com>
2013-10-12 22:17:00 +0900

    init project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ndigits
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/ndigits/core_test.clj
@@ -0,0 +1,7 @@
+(ns ndigits.core-test
+  (:require [clojure.test :refer :all]
+            [ndigits.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
