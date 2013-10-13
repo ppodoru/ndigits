10cbee7c6a71660a895c85382cf7a45117e08f16
ppodoru <ppodoru@navercorp.com>
2013-10-13 17:37:48 +0900

    버전 update. 0.1.1-SNAPSHOT 개발 시작

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ndigits "0.1.0"
+(defproject ndigits "0.1.1-SNAPSHOT"
   :description "clojure text game"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
