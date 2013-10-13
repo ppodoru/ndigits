457f5848bbba8c2002287ce2d9698a862ddc90ce
ppodoru <ppodoru@navercorp.com>
2013-10-13 17:34:36 +0900

    release 버전(0.1.0)으로 수정 (SNAPSHOT 제거)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ndigits "0.1.0-SNAPSHOT"
+(defproject ndigits "0.1.0"
   :description "clojure text game"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
